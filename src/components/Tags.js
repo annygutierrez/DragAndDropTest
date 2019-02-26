d1ca26783a4e9d7b3963c09db76f03760908b8aa
Anny Guttierrez <agutilop@everis.com>
2019-02-26 00:37:09 -0500

    Add drag and drop animation
    - Add drag and drop animation.

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,11 +2,12 @@ import React, { PureComponent } from 'react';
 import {
     LayoutAnimation,
     UIManager,
+    PanResponder,
     StyleSheet,
     View
 } from 'react-native';
 import TagsArea from './TagsArea';
-import type { TagObject } from '../types';
+import type { TagObject, GestureState } from '../types';
 
 UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
 
@@ -38,6 +39,15 @@ export default class Tags extends PureComponent {
             .map((title: string) => ({ title })),   // convert to objects
     };
 
+    // PanResponder to handle drag and drop gesture
+    panResponder: PanResponder;
+
+    // Initialize PanResponder
+    componentWillMount() {
+        this.panResponder = this.createPanResponder();
+    }
+
+
     // Animate layout changes when dragging or removing a tag
     componentWillUpdate() {
         LayoutAnimation.configureNext({
@@ -45,6 +55,39 @@ export default class Tags extends PureComponent {
             duration: this.props.animationDuration
         });
     }
+
+    // Create PanResponder
+    createPanResponder = (): PanResponder => PanResponder.create({
+        // Handle drag gesture
+        onMoveShouldSetPanResponder: (_, gestureState: GestureState) => this.onMoveShouldSetPanResponder(gestureState),
+        onPanResponderGrant: (_, gestureState: GestureState) => this.onPanResponderGrant(),
+        onPanResponderMove: (_, gestureState: GestureState) => this.onPanResponderMove(gestureState),
+        // Handle drop gesture
+        onPanResponderRelease: (_, gestureState: GestureState) => this.onPanResponderEnd(),
+        onPanResponderTerminate: (_, gestureState: GestureState) => this.onPanResponderEnd(),
+    });
+
+    // Find out if we need to start handling tag dragging gesture
+    onMoveShouldSetPanResponder = (gestureState: GestureState): boolean => {
+        console.log('setting PanResponder');
+        return true;
+    };
+
+    // Called when gesture is granted
+    onPanResponderGrant = (): void => {
+        console.log('granted');
+    };
+
+    // Handle drag gesture
+    onPanResponderMove = (gestureState: GestureState): void => {
+        const { moveX, moveY } = gestureState;
+        console.log('onPanResponderMove', moveX, moveY);
+    };
+
+    // Called after gesture ends
+    onPanResponderEnd = (): void => {
+        console.log('ended');
+    };
 
     // Remove tag
     removeTag = (tag: TagObject): void => {
@@ -65,6 +108,7 @@ export default class Tags extends PureComponent {
         return (
             <View
                 style={styles.container}
+                {...this.panResponder.panHandlers}
             >
 
                 <TagsArea
