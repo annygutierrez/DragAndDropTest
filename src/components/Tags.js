df31d26c9482631a4af9ca6c19879b9897cc77c6
Anny Guttierrez <agutilop@everis.com>
2019-02-26 00:23:46 -0500

    Add layout animation functionality
    - Add layout animation functionality.

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,56 +1,88 @@
 import React, { PureComponent } from 'react';
 import {
-  StyleSheet,
-  View
+    LayoutAnimation,
+    UIManager,
+    StyleSheet,
+    View
 } from 'react-native';
 import TagsArea from './TagsArea';
 import type { TagObject } from '../types';
 
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
+
 type Props = {
-  // Array of tag titles
-  tags: string[],
-  // Passes onPressAddNewTag callback down to TagsArea component
-  onPressAddNewTag: () => void,
+    // Array of tag titles
+    tags: string[],
+    // Tag swapping animation duration in ms
+    animationDuration: number,
+    // Passes onPressAddNewTag callback down to TagsArea component
+    onPressAddNewTag: () => void,
 };
 
 type State = {
-  tags: TagObject[],
+    tags: TagObject[],
 };
 
 export default class Tags extends PureComponent {
 
-  props: Props;
+    props: Props;
 
-  state: State = {
-    // Convert passed array of tag titles to array of objects of TagObject type,
-    // so ['tag', 'another'] becomes [{ title: 'tag' }, { title: 'another' }]
-    tags: [...new Set(this.props.tags)]       // remove duplicates
-      .map((title: string) => ({ title })),   // convert to objects
-  };
+    static defaultProps = {
+        animationDuration: 250
+    };
 
-  render() {
-    const { tags } = this.state;
-    return (
-      <View
-        style={styles.container}
-      >
+    state: State = {
+        // Convert passed array of tag titles to array of objects of TagObject type,
+        // so ['tag', 'another'] becomes [{ title: 'tag' }, { title: 'another' }]
+        tags: [...new Set(this.props.tags)]       // remove duplicates
+            .map((title: string) => ({ title })),   // convert to objects
+    };
 
-        <TagsArea
-          tags={tags}
-          onPress={() => {}} // do nothing for now
-          onRenderTag={() => {}} // do nothing for now
-          onPressAddNew={this.props.onPressAddNewTag}
-        />
+    // Animate layout changes when dragging or removing a tag
+    componentWillUpdate() {
+        LayoutAnimation.configureNext({
+            ...LayoutAnimation.Presets.easeInEaseOut,
+            duration: this.props.animationDuration
+        });
+    }
 
-      </View>
-    );
-  }
+    // Remove tag
+    removeTag = (tag: TagObject): void => {
+        this.setState((state: State) => {
+            const index = state.tags.findIndex(({ title }) => title === tag.title);
+            return {
+                tags: [
+                    // Remove the tag
+                    ...state.tags.slice(0, index),
+                    ...state.tags.slice(index + 1),
+                ]
+            }
+        });
+    };
+
+    render() {
+        const { tags } = this.state;
+        return (
+            <View
+                style={styles.container}
+            >
+
+                <TagsArea
+                    tags={tags}
+                    onPress={this.removeTag}
+                    onRenderTag={() => { }} // do nothing for now
+                    onPressAddNew={this.props.onPressAddNewTag}
+                />
+
+            </View>
+        );
+    }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 15,
-  },
+    container: {
+        flex: 1,
+        paddingHorizontal: 15,
+    },
 });
